db1fe672979afbb3d3d644ce7d7f9e01bc2f6d0b
MJJ <m3k0813@gmail.com>
2025-01-28 22:21:33 +0900

    feat(SCRUM-122): 초기 로그인 시 유저 노드 생성

--- src/main/java/com/team_nebula/nebula/domain/user/entity/UserNode.java
+++ src/main/java/com/team_nebula/nebula/domain/user/entity/UserNode.java
@@ -1,32 +1,37 @@
 package com.team_nebula.nebula.domain.user.entity;
 
-import com.team_nebula.nebula.domain.category.entity.Category;
-import com.team_nebula.nebula.domain.common.BaseEntity;
-import com.team_nebula.nebula.domain.star.entity.Star;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Relationship;
 
-import java.util.HashSet;
-import java.util.Set;
+import com.team_nebula.nebula.domain.category.entity.Category;
+import com.team_nebula.nebula.domain.common.BaseEntity;
+import com.team_nebula.nebula.domain.star.entity.Star;
 
-@Getter
-@Setter
-@NoArgsConstructor
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 @Node
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserNode extends BaseEntity {
 
-    @Id
-    private Long userId;
+	@Id
+	private Long userId;
 
-    @Relationship(type = "CREATED", direction = Relationship.Direction.OUTGOING)
-    private Set<Star> stars = new HashSet<>();
+	@Relationship(type = "CREATED", direction = Relationship.Direction.OUTGOING)
+	private Set<Star> stars = new HashSet<>();
 
-    @Relationship(type = "GENERATED", direction = Relationship.Direction.OUTGOING)
-    private Set<Category> categorySet = new HashSet<>();
+	@Relationship(type = "GENERATED", direction = Relationship.Direction.OUTGOING)
+	private Set<Category> categorySet = new HashSet<>();
 
-
+	@Builder
+	public UserNode(Long userId) {
+		this.userId = userId;
+	}
 }
