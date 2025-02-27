0a0e558931252b7d3e1751e5c17e848860fa2ee2
MJJ <m3k0813@gmail.com>
2025-02-27 13:47:55 +0900

    feat(SCRUM-143):  스웨거 AuthUser 어노테이션 무시

--- src/main/java/com/team_nebula/nebula/global/config/SwaggerConfig.java
+++ src/main/java/com/team_nebula/nebula/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.team_nebula.nebula.global.config;
 
+import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.team_nebula.nebula.global.annotation.AuthUser;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -11,8 +14,12 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 @Configuration
 public class SwaggerConfig {
 
+	static {
+		SpringDocUtils.getConfig().addAnnotationsToIgnore(AuthUser.class);
+	}
 	@Bean
 	public OpenAPI openAPI() {
+
 		SecurityScheme securityScheme = new SecurityScheme()
 			.type(SecurityScheme.Type.HTTP)
 			.scheme("bearer")
