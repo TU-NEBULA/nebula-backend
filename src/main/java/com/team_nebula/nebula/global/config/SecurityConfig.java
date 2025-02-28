adbe5f08c7e66ea59f17dc6ce9b8260f79994121
MJJ <m3k0813@gmail.com>
2025-02-28 16:07:53 +0900

    feat(SCRUM-143): 임시 토큰 발급 security 해제

--- src/main/java/com/team_nebula/nebula/global/config/SecurityConfig.java
+++ src/main/java/com/team_nebula/nebula/global/config/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
 		//경로별 인가 작업
 		http
 			.authorizeHttpRequests((auth) -> auth
-				.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/h2-console/**").permitAll()
+				.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/h2-console/**", "/api/v1/oauth/generate").permitAll()
 				.anyRequest().authenticated());
 
 		//세션 설정 : STATELESS
