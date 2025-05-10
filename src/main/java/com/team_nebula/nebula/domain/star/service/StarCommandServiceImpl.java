2cdb6c37ace1cac11a9885820d4d31f8a4202bc7
SeungkiHwang <tmdrl7777@gmail.com>
2025-05-11 05:17:26 +0900

    refactor(SCRUM-208) : 스타 저장 후 AI서버에 데이터 전송 기능 추가

--- src/main/java/com/team_nebula/nebula/domain/star/service/StarCommandServiceImpl.java
+++ src/main/java/com/team_nebula/nebula/domain/star/service/StarCommandServiceImpl.java
@@ -274,6 +274,7 @@ public class StarCommandServiceImpl implements StarCommandService {
 
         // 유저 스타 작업 횟수 증가
 //        aiService.checkUpdatedCnt(userId);
+        aiMessageService.sendStarData(userId, lastStar, requestDTO.getS3key(), lastStar.getUserMemo(), lastStar.getSummaryAI(), requestDTO.getKeywordList());
 
         return CreateStarResponseDTO.builder()
                 .starId(lastStar.getId())
