2770f6a1000fe215cf37b9a5621633a576418c8d
JJM <m3k0813@gmail.com>
2025-05-11 01:54:43 +0900

    feature(SCRUM-198): 챗봇 DynamoDB 연동

--- src/main/java/com/team_nebula/nebula/domain/chatbot/service/ChatbotServiceImpl.java
+++ src/main/java/com/team_nebula/nebula/domain/chatbot/service/ChatbotServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.team_nebula.nebula.domain.chatbot.dto.response.ChatResponseDTO;
+import com.team_nebula.nebula.domain.chatbot.entity.ChatBot;
+import com.team_nebula.nebula.domain.chatbot.repository.ChatbotRepository;
 import com.team_nebula.nebula.global.AI.service.AiMessageService;
 
 import lombok.RequiredArgsConstructor;
@@ -13,10 +15,24 @@ import lombok.RequiredArgsConstructor;
 public class ChatbotServiceImpl implements ChatbotService {
 
 	private final AiMessageService aiMessageService;
+	private final ChatbotRepository chatbotRepository;
 
 	@Override
 	@Transactional
 	public ChatResponseDTO chat(Long userId, String prompt) {
+
+		ChatBot chatBot = ChatBot.builder()
+			.chatId("1")
+			.title("test")
+			.content("test")
+			.userId("1")
+			.build();
+
+		chatbotRepository.saveChat(chatBot);
+
+		ChatResponseDTO response = ChatResponseDTO.builder()
+			.message("성공")
+			.build();
 
 		ChatResponseDTO response = aiMessageService.chatBot(userId, prompt);
 		return response;
