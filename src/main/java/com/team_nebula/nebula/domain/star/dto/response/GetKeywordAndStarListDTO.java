09a6b8144d19559f72b09925785288b25831d036
SeungkiHwang <tmdrl7777@gmail.com>
2025-06-15 14:49:25 +0900

    feature(SCRUM-227) : Dto 정의

---
+++ src/main/java/com/team_nebula/nebula/domain/star/dto/response/GetKeywordAndStarListDTO.java
@@ -0,0 +1,17 @@
+package com.team_nebula.nebula.domain.star.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetKeywordAndStarListDTO {
+    private String keyword;
+    private List<GetStarOneResponseDTO> starList;
+}
