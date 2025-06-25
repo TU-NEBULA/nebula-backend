202ec9dd7ccd58a92545d8d069a5444471253cca
SeungkiHwang <tmdrl7777@gmail.com>
2025-06-25 18:51:58 +0900

    feature(SCRUM-229) : Elasticsearch Config 구현

---
+++ src/main/java/com/team_nebula/nebula/global/config/ElasticsearchConfig.java
@@ -0,0 +1,25 @@
+package com.team_nebula.nebula.global.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.team_nebula.nebula.domain.star.search.repository")
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.uris}")
+    private String[] elasticsearchUris;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(elasticsearchUris)
+                .withConnectTimeout(10000)
+                .withSocketTimeout(60000)
+                .build();
+    }
+}
